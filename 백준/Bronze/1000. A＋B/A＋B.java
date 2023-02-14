53f45720c1373cb2191791aa44399dd6cc8d0f42
kim9757di <79069497+kim9757di@users.noreply.github.com>
2023-02-14 15:10:53 +0900

    [Bronze V] Title: A+B, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		/*    
+		굳이 String 변수 생성 안하고 입력과 동시에 구분자로 분리해줘도 된다.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		*/
+		System.out.println(a + b);
+	}
+
+}
